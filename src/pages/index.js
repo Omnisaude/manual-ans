c79222f85daa03abc8df60e9ced6bd370b32559f
Rafael Reis <rafareisbr@gmail.com>
2022-10-26 11:32:56 -0300

    update tela inicial

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,35 +17,35 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: 'Easy to Use',
+    title: 'Fácil de Usar',
     imageUrl: 'img/undraw_docusaurus_mountain.svg',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Funções simples, uso intuitivo. Foco em manter a simplicidade desde sua
+        concepção.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Foco no que importa',
     imageUrl: 'img/undraw_docusaurus_tree.svg',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Nossa documentação automatizada facilita que você encontre as
+        informações que você procura mais facilmente.
       </>
     ),
   },
-  {
-    title: 'Powered by React',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+  // {
+  //   title: 'Powered by React',
+  //   imageUrl: 'img/undraw_docusaurus_react.svg',
+  //   description: (
+  //     <>
+  //       Extend or customize your website layout by reusing React. Docusaurus can
+  //       be extended while reusing the same header and footer.
+  //     </>
+  //   ),
+  // },
 ];
 
 function Feature({imageUrl, title, description}) {
